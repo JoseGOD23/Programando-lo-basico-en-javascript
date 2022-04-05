b9fd3b9fabf69241f0785ea710ab79441849ab86
JoseGOD23 <103016110+JoseGOD23@users.noreply.github.com>
2022-04-05 18:10:15 -0500

    Add files via upload

---
+++ ¿calladown.js
@@ -0,0 +1,85 @@
+"JoseGOD anda de malas".trim()
+"calla down" .trim()
+"este pete que" .trim()
+"cerrar el orto esclavo" .trim()
+"calla miembro de legion holk" .trim()
+"1 ,2 ,3 ,4 ,5 ,6 ,7";
+" 1.2.3.4.5.6.7.8.9" toUpperCase();
+"con que eren jaegger pues ahora eren se va" toUpperCase();
+"calla ateo" toUpperCase();
+"ola petelan" toUpperCase();
+"bienvenido al infierno"();
+"calla down" toUpperCase();
+"este mierdon" toLowerCase();
+"lul" toLowerCase();
+"ateista de mierda" toLowerCase();
+"duendebv".length;
+"el xmen mas imbecil".length;
+
+12+34;
+Outpout
+123456;
+23574334;
+234(3+4)-5;
+duende es gay =false;
+var duende es gay = true;
+duende es gay;
+true 
+me gusta la cocada = false;
+var me gusta la cocada = true;
+me gusta la cocada;
+true 
+"calla down"toUpperCase();
+56 + 23;
+"jose" + "god";
+var dinner;"duendegod";
+var viento = "caluroso";
+viento = "ventiscoso";
+viento;
+function theskyissobig(){
+	return "Hello JoseGOD":
+}
+theskyissobig();
+function josegodesgay(){
+	return "duendeeshetero":
+}
+josegodesgay();
+if (false){
+	var outcome = "if block";
+} else if (true){
+	var outcome = "first else if block";
+} else if (true){
+	var outcome = "second else if block";
+} else {
+	var outcome = "else block";
+}
+outcome ;
+
+var brothers = ["Tia","Tamara"];
+brothers [0];
+outpout 
+var uncles = ["Sam","Jose"];
+uncles [0];
+outpout
+
+var course ={
+	name:"Grado 221"
+	start:9,
+	end:12
+};
+course.name;
+outpout
+
+var course ={
+	name:"Grado 213"
+	start:8,
+	end: 13
+};
+course.name;
+outpout
+
+	
+
+
+
+
